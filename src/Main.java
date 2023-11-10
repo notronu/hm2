ec9b4f7581c090bcea65f09beb99351c5b072882
Chika_Pyu <ya.sasha-mochalov1@yandex.ru>
2023-11-10 13:07:26 +0300

    hm2

---
+++ src/Main.java
@@ -0,0 +1,86 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача 2");
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача 3");
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        System.out.println("Задача 6");
+        var x = 78.2;
+        var y = 82.7;
+        var result = x + y;
+        System.out.println ("x + y = " + result );
+        result = y - x;
+        System.out.println ("y - x = " + result );
+
+        System.out.println("Задача 7");
+        x = 78.2;
+        y = 82.7;
+        result = y - x;
+        System.out.println ("y - x = " + result );
+        var overload = (y + x) % result;
+        System.out.println(" разница " + overload + " кг!");
+
+        System.out.println("Задача 8");
+        var allHours = 640;
+        var eachEmployee = 8;
+        result = allHours / eachEmployee;
+        System.out.println("Всего работников в компании " + result + "человек" );
+        var b = result + 94;
+        result = allHours / b;
+        System.out.println("Если в компании работает" + b + "человек" + result + "часов работы может быть поделено между сотрудниками");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
